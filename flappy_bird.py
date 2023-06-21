497a25d81ecd81bdcfa4e0369543cdca344db096
leon <l.elliottfuller@gmail.com>
2023-06-21 18:19:20 +0200

    Created the Bird class & constructor

--- flappy_bird.py
+++ flappy_bird.py
@@ -15,4 +15,21 @@ PIPE_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", 
 BASE_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "base.png")))]
 BACKGROUND_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bg.png")))]
 
-
+class Bird:
+	IMAGES = BIRD_IMAGES
+	MAX_ROTATION = 25
+	ROTATION_VELOCITY = 20
+	ANIMATION_TIME = 5
+	
+	def __init(self, x, y):
+		self.x = x
+		self.y = y
+		self.tilt = 0
+		self.tick_count = 0
+		self.velocity = 0
+		self.height = self.y
+		self.image_count = 0
+		self.image = self.IMAGES[0]
+		
+	def jump(self):
+		self.velocity = -10.5
