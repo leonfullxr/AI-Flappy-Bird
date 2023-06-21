52f464a7968d91feda0eb3266409eab2df6dd662
leon <l.elliottfuller@gmail.com>
2023-06-21 18:13:40 +0200

    Declared the images for the game

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,3 +4,15 @@ import time
 import os
 import random
 
+WIN_HEIGHT = 800
+WIN_WIDTH = 600
+
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird1.png")))],
+	      	  [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird2.png")))],
+			  [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird3.png")))]
+			  
+PIPE_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "pipe.png")))]
+BASE_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "base.png")))]
+BACKGROUND_IMAGE = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bg.png")))]
+
+
